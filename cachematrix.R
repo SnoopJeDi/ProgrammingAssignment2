b4e2ee5b65e12341fa675789d429d5ca9d76401f
SnoopJeDi <snoopjedi@gmail.com>
2015-11-22 21:33:59 -0600

    cachematrix assignment done

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,13 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- list(matrix = x)
+  inverse <- NULL ## *NOT* a part of the object itself, but part of the associated ENVIRONMENT, with a getter/setter
+  mat$setInverse <- function(y) {
+    inverse <<- y ## set in parent scope which is mat's environment
+  }
+  mat$getInverse <- function() { return( inverse ) }
+  return(mat)
 }
 
 
@@ -12,4 +18,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  if ( is.null( x$getInverse() ) ) {
+    ## no cached inverse, so get one and store it
+    x$setInverse( solve(x$matrix) )
+  }
+  
+  return( x$getInverse() )
 }
